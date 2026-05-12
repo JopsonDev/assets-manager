b6941eab1e88681db2ded921971cf3d31e6f85d6
JopsonDev <jopsonmason@outlook.com>
2026-05-12 13:05:12 -0400

    added test to test car price

---
+++ src/test/java/com/pluralsight/VehicleTest.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @Test
+    public void getValue_ValueDecreaseBaseOnYear_ValueDecreases(){
+        //Arrange
+        Vehicle car = new Vehicle("sad", "asd", 100, "volswagen jetta", 2020, 5000);
+        double expectedPrice = 100 * (1 - .18);
+
+        //Act
+        double actualPrice = car.getValue();
+
+        //Assert
+        assertEquals(expectedPrice, actualPrice);
+    }
+
+    @Test
+    public void getValue_ExtraDecreaseBasedOnMilage_ValueDecreases(){
+        //Arrange
+        Vehicle car = new Vehicle("sad", "asd", 100, "volswagen jetta", 2026, 105000);
+        double expectedPrice = 100 * .75;
+
+        //Act
+        double actualPrice = car.getValue();
+
+        //Assert
+        assertEquals(expectedPrice, actualPrice);
+
+
+    }
+
+    @Test
+    public void getValue_NoMilageReductionBasedOnCarMake_ValueRemainsSame(){
+        //Arrange
+        Vehicle car = new Vehicle("sad", "asd", 100, "honda jetta", 2026, 105000);
+        Vehicle car2 = new Vehicle("sad", "asd", 100, "Toyota jetta", 2026, 105000);
+        double expectedPrice = 100;
+
+        //Act
+        double actualPrice = car.getValue();
+        double actualPrice2 = car2.getValue();
+
+        //Assert
+        assertEquals(expectedPrice, actualPrice);
+        assertEquals(expectedPrice, actualPrice2);
+
+
+    }
+}
